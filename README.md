493f914c9d5727be0ee857f0d0e7488eb2f55375
John Bohannon <imjohnbo@github.com>
2019-10-24 20:47:47 -0400

    Feat: add pinned input to enable pinning the issue

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Weekly Radar
 
-:star: Updated for [the latest version](https://github.blog/2019-08-08-github-actions-now-supports-ci-cd/) of [GitHub Actions](https://help.github.com/en/categories/automating-your-workflow-with-github-actions) :star: 
-
 ## Usage
-Use this GitHub Action in your team's repository to stay in touch about your plans for the week. Weekly Radar will find the first open issue with `label`, close it, and link it to a newly created Weekly Radar template issue. If there is no open issue with `label`, it will create one. Finally, it will assign the new issue to `assignees`.
+
+Work on a distributed team? Try using Weekly Radar as a [stand up](https://en.wikipedia.org/wiki/Stand-up_meeting) process automation bot to keep track of what you're all working on. 🚀
+
+Weekly Radar is GitHub Action that will find the first open issue with `label`,
+close it, and link it to a newly created Weekly Radar template issue. If there
+is no open issue with `label`, it will create one. It will also assign the
+new issue to `assignees` and [pin the issue](https://help.github.com/en/github/managing-your-work-on-github/pinning-an-issue-to-your-repository) (`pinned` = `true`) for easier access.
 
 **Example workflow**:
 
@@ -11,7 +15,7 @@ Use this GitHub Action in your team's repository to stay in touch about your pla
 name: Weekly Radar
 on:
   schedule:
-  - cron: 0 12 * * 1
+  - cron: 0 12 * * 1  # https://crontab.guru
 
 jobs:
   weekly_radar:
@@ -24,6 +28,7 @@ jobs:
       with:
         assignees: "some space delimited list of assignees"
         label: "my-custom-label"
+        pinned: true
       env:
         GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
 ```
@@ -34,18 +39,29 @@ jobs:
 
 ## Miscellaneous
 
-* `schedule(*,*,*,*,*)` is just [one option](https://help.github.com/en/articles/events-that-trigger-workflows#scheduled-events) based on POSIX cron syntax. Knock yourself out by instead responding to [events](https://help.github.com/en/articles/events-that-trigger-workflows)!
+- `schedule(*,*,*,*,*)` is just
+  [one option](https://help.github.com/en/articles/events-that-trigger-workflows#scheduled-events)
+  based on POSIX cron syntax. Knock yourself out by instead responding to
+  [events](https://help.github.com/en/articles/events-that-trigger-workflows)!
 
 ## Inputs
-* `assignees` (optional) is a space delimited list of assignees for the Weekly Radar.
-* `label` (optional) is the label to be attached to the Weekly Radar.
 
-## Environment variables 
-* GITHUB_TOKEN is assigned the [automatically-generated GitHub token](https://help.github.com/en/articles/virtual-environments-for-github-actions#creating-and-using-secrets-encrypted-variables) that is scoped for the repository whose workflow calls the Action.
+- `assignees` (optional) is a space delimited list of assignees for the Weekly
+  Radar.
+- `label` (optional) is the label to be attached to the Weekly Radar.
+- `pinned` (optional) is a boolean flag that [pins the issue](https://help.github.com/en/github/managing-your-work-on-github/pinning-an-issue-to-your-repository) to your repository for easier access.
+
+## Environment variables
+
+- `GITHUB_TOKEN` is assigned the
+  [automatically-generated GitHub token](https://help.github.com/en/articles/virtual-environments-for-github-actions#creating-and-using-secrets-encrypted-variables)
+  that is scoped for the repository whose workflow calls the Action.
 
 ## Contributing
-Feel free to open an issue, or better yet, a [pull request](https://github.com/imjohnbo/weekly-radar/compare)!
+
+Feel free to open an issue, or better yet, a
+[pull request](https://github.com/imjohnbo/weekly-radar/compare)!
 
 ## License
-[MIT](https://choosealicense.com/licenses/mit/)
 
+[MIT](https://choosealicense.com/licenses/mit/)
--- index.js
+++ index.js
@@ -1,71 +1,158 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const assigneesStr = core.getInput('assignees');
-const labelStr = core.getInput('label');
-// This should be a token with access to your repository scoped in as a secret.
-const githubToken = process.env.GITHUB_TOKEN;
-const octokit = new github.GitHub(githubToken);
+
+const token = process.env.GITHUB_TOKEN;
+const octokit = new github.GitHub(token);
 const repo = process.env.GITHUB_REPOSITORY;
-const getCurrentRadarStr = `{
-  resource(url: "${repo}") {
-    ... on Repository {
-      issues(first:1, labels:["${labelStr}"], states:[OPEN]) {
-        nodes {
-          number
+
+// Given a GraphQL issue id, unpin the issue
+const unpin = issueId => {
+  const mutation = `mutation {
+    unpinIssue(input: {issueId: "${issueId}"}) {
+      issue {
+        body
+      }
+    }
+  }`;
+  return octokit.graphql({
+    query: mutation,
+    headers: {
+      accept: `application/vnd.github.elektra-preview+json`,
+    },
+  });
+};
+
+// Given a GraphQL issue id, pin the issue
+const pin = issueId => {
+  const mutation = `mutation {
+    pinIssue(input: {issueId: "${issueId}"}) {
+      issue {
+        body
+      }
+    }
+  }`;
+  return octokit.graphql({
+    query: mutation,
+    headers: {
+      accept: `application/vnd.github.elektra-preview+json`,
+    },
+  });
+};
+
+async function run() {
+  try {
+    const assignees = core.getInput('assignees').split(' ');
+    const label = core.getInput('label');
+    const pinned = core.getInput('pinned') === 'true';
+  
+    // Form date long to short, ex. 2019-10-21
+    const today =
+      (new Date()).getFullYear() +
+      '-' +
+      ('0' + ((new Date()).getMonth() + 1)).slice(-2) +
+      '-' +
+      ('0' + (new Date()).getDate()).slice(-2);
+  
+    // GraphQL query to get latest open weekly radar issue if it exists
+    const latestRadarQuery = `{
+      resource(url: "${repo}") {
+        ... on Repository {
+          issues(first:1, labels:["${label}"], states:[OPEN]) {
+            nodes {
+              number
+              id
+            }
+          }
+        }
+      }
+    }`;
+  
+    // Run the query, save the number (ex. 79) and GraphQL id (ex. MDU6SXMzbWU0ODAxNzI0NDA=)
+    const {
+      number: currentRadarNumber,
+      id: currentRadarId,
+    } = (await octokit.graphql(latestRadarQuery)).resource.issues.nodes[0] || {};
+  
+    core.debug(`Current radar issue number: ${currentRadarNumber}`);
+
+    let template = 
+`:wave: Hi there!
+    
+### What are you focusing on this week?`;
+  
+template += currentRadarNumber
+
+? `
+
+Previously: #${currentRadarNumber}`
+
+: ``;
+  
+    // Create a new radar
+    const { data: { number: newRadarNumber } } = await octokit.issues.create({
+      ...github.context.repo,
+      title: `Weekly Radar, week of ${today}`,
+      body: template,
+      labels: [label],
+      assignees: assignees,
+    }) || {};
+
+    core.debug(`New radar issue number: ${newRadarNumber}`);
+
+    const repositoryByNumberQuery = `{
+      resource(url: "${repo}") {
+        ... on Repository {
+          issue(number: ${newRadarNumber}){
+            id
+          }
+        }
+      }
+    }`;
+  
+    // Query to get the GraphQL id (ex. MDX6SXMzbWU0ODAxNzI0NDA=) of the new issue that we have the number of
+    const { id: newRadarId } = (await octokit.graphql(
+      repositoryByNumberQuery,
+    )).resource.issue || {};
+  
+    // If there is a current weekly radar, close it out and point to the new
+    if (currentRadarNumber) {
+      // Create comment on the current that points to the new
+      await octokit.issues.createComment({
+        ...github.context.repo,
+        issue_number: currentRadarNumber,
+        body: `Next: #${newRadarNumber}`,
+      });
+  
+      // Close out the current
+      await octokit.issues.update({
+        ...github.context.repo,
+        issue_number: currentRadarNumber,
+        state: 'closed',
+      });
+  
+      // If the pinned input is true, unpin the current
+      if (pinned) {
+        try {
+          core.debug(`Unpinning ${currentRadarId}...`);
+          await unpin(currentRadarId);
+        }
+        catch(error) {
+          core.debug(`Pinning ${newRadarId}...`);
+          await pin(newRadarId);
         }
       }
     }
+  
+    // If the pinned input is true, pin the new
+    if (pinned) {
+      core.debug(`newRadarId: ${newRadarId}`);
+      await pin(newRadarId);
+    }
   }
-}`;
-const updateCurrentRadarStr = (next) => {
-  return `Next: #${next}`;
-};
-const createNewRadarStr = (prev) => {
-  let str = `:wave: Hi there!
-
-  ### What are you focusing on this week?`;
-
-  str += !!prev ? `
-  
-  Previously: #${prev}` : ``;
-  
-  return str;
-};
-
-async function exec() {
-  const today = new Date();
-  const assignees = assigneesStr.split(' ');
-  const currentRadar = (await octokit.graphql(getCurrentRadarStr)).resource.issues.nodes[0];
-  const currentRadarId = !!currentRadar ? currentRadar.number : undefined;
-  const dateString = today.getFullYear() + '-' + ('0' + (today.getMonth()+1)).slice(-2) + '-' + ('0' + today.getDate()).slice(-2);
-
-  // create new radar
-  const newRadar = await octokit.issues.create({
-    ...github.context.repo,
-    title: `Weekly Radar, week of ${dateString}`,
-    body: createNewRadarStr(currentRadarId),
-    labels: [labelStr],
-    assignees: assignees
-  });
-  
-  const newRadarId = newRadar.data.number;
-
-  if (!!currentRadarId) {
-    // create comment on the old that points to the new
-    await octokit.issues.createComment({
-      ...github.context.repo,
-      issue_number: currentRadarId,
-      body: updateCurrentRadarStr(newRadarId)
-    });
-  
-    // close out the old
-    await octokit.issues.update({
-      ...github.context.repo,
-      issue_number: currentRadarId,
-      state: 'closed'
-    });
+  catch(error) {
+    core.error(`Error encountered: ${error}.`);
   }
-
+  
 }
 
-exec();
+run();
